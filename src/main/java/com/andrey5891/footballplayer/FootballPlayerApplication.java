bf95993356093611f768615703a731a315ce299a
Korchenkov <kc@kode.ru>
2024-05-04 12:41:43 +0300

    created spring boot project

---
+++ src/main/java/com/andrey5891/footballplayer/FootballPlayerApplication.java
@@ -0,0 +1,13 @@
+package com.andrey5891.footballplayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballPlayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballPlayerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andrey5891/footballplayer/FootballPlayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrey5891.footballplayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballPlayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
